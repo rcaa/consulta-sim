45cf3c7a7289d7baf229bd568c1d1ad3207a4d20
Luizfdarb <78767627+Luizfdarb@users.noreply.github.com>
2023-04-01 15:36:16 -0300

    Criação dost testes de model de consulta
    Dois foram criados, conforme solicitado.

--- test/models/consulta_test.rb
+++ test/models/consulta_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class ConsultaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar consulta sem paciente" do
+    consultas = Consulta.new
+    consultas.medico_id = 1
+    consultas.data = "2018-10-10"
+    consultas.horario = "10:00"
+    assert_not consultas.save, "Salvou consulta sem paciente"
+  end
+
+  test "criar consulta sem medico" do
+    consultas = Consulta.new
+    consultas.paciente_id = 1
+    consultas.data = "2018-10-10"
+    consultas.horario = "10:00"
+    assert_not consultas.save, "Salvou consulta sem medico"
+  end
 end
