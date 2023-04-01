8cde80f9e99957fa42065d5e1c74ab3d2125b41a
Luizfdarb <78767627+Luizfdarb@users.noreply.github.com>
2023-04-01 15:35:16 -0300

    Criação dost testes de model de paciente
    Dois foram criados, conforme solicitado.

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "tentar salvar paciente sem endereco" do
+    paciente = Paciente.new
+    paciente.nome = "João da Silva"
+    paciente.data_nascimento = "2021-08-12"
+    paciente.cpf = "12345678901"
+    paciente.email = "joao@gmail.com"
+
+    assert_not paciente.save, "Erro: Endereco nao pode ficar vazio"
+  end
+
+  test "salvar paciente sem nome" do
+    paciente = Paciente.new
+    paciente.nome = ""
+    paciente.data_nascimento = "2021-08-12"
+    paciente.cpf = "12345678901"
+    paciente.email = "teste@teste"
+
+
+    enderecos = Endereco.new
+    enderecos.logradouro = "Rua 1"
+    enderecos.complemento = "Casa 1"
+    enderecos.cep = "12345678"
+    enderecos.bairro = "Centro"
+    enderecos.cidade = "São Paulo"
+
+    paciente.endereco = enderecos
+    assert_not paciente.save, "Salvou paciente sem nome"
+  end
 end
