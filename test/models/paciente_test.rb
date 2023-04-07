761bb3a963f218b69e3ffa050155853caa4e52aa
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-04-07 18:23:09 -0300

    testes de model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Salvar o paciente sem atributos" do
+    @paciente = Paciente.new
+    assert_not @paciente.save
+  end
+
+  test "Salvar paciente com todos os atributos validos" do
+    paciente = Paciente.new nome: 'Rodrigo',
+                            cpf:'70383167450',
+                            email: "rodrigo@gmail.com",
+                            data_nascimento: "2002-06-05",
+                            endereco_attributes:{ cep: "55298-165",
+                            logradouro: "rua do zekinha",
+                            cidade: 'Garanhuns',
+                            bairro: "Heliopolis",
+                            complemento: 'posto',
+                            }
+    assert paciente.save
+  end
 end
