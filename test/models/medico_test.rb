fbca289a83013f337ddc428deb71760929e44a9d
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-04-07 18:34:45 -0300

    testes de model de medico

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -2,8 +2,21 @@ require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
   test 'criando medico corretamente' do
-    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
-                        especialidade: 'Reumatologista', crm: '123456'
+    medico = Medico.new nome: 'Rodrigo',
+                        cpf: '68551863940',
+                        email: 'rodrigo@rodrigo.com',
+                        especialidade: 'Cardiologista',
+                        crm: '123456'
     assert medico.save
+  end
+
+  test 'excluindo medico corretamente' do
+    medico = Medico.new nome: 'Rodrigo Leandro',
+                        cpf: '08283544217',
+                        email: 'rodrigo@gmail.com',
+                        especialidade: 'Reumatologista',
+                        crm: '123456'
+    assert medico.save
+    assert medico.destroy
   end
 end
