7436ac584aaa3cf1df488afdf08ff8485eb72848
Gison <gisonvilaca@gmail.com>
2023-04-07 18:18:09 -0300

    Testes de Model de Paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Criando conta de paciente sem atributos" do
+    paciente = Paciente.new
+    assert_not paciente.save
+  end
+
+
+  test "Criando conta de paciente com atributos validos" do
+    paciente = Paciente.new nome: 'Gison Vilaca',
+                            data_nascimento: "2001-04-25",
+                            cpf: '711.785.718-82',
+                            email: 'vilaca@gmail.com',
+                            endereco_attributes:{
+                              logradouro: 'Rua da Escola',
+                              complemento: 'Casa',
+                              cep: '55290-000',
+                              bairro: 'Parque Alvorada',
+                              cidade: 'Sao Joao'
+    }
+    assert paciente.save
+  end
 end
