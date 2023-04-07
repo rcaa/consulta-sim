b7fb57e616c304d25e80e7954c307bb5d94f55b3
Rodrigo Leandro <rodrigo.leandro.zx@gmail.com>
2023-04-07 19:11:38 -0300

    ajustando testes de model

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -3,19 +3,19 @@ require "test_helper"
 class MedicoTest < ActiveSupport::TestCase
   test 'criando medico corretamente' do
     medico = Medico.new nome: 'Rodrigo',
-                        cpf: '68551863940',
-                        email: 'rodrigo@rodrigo.com',
-                        especialidade: 'Cardiologista',
-                        crm: '123456'
+             cpf: '68551863940',
+             email: 'rodrigo@rodrigo.com',
+             especialidade: 'Cardiologista',
+             crm: '123456'
     assert medico.save
   end
 
   test 'excluindo medico corretamente' do
     medico = Medico.new nome: 'Rodrigo Leandro',
-                        cpf: '08283544217',
-                        email: 'rodrigo@gmail.com',
-                        especialidade: 'Reumatologista',
-                        crm: '123456'
+             cpf: '08283544217',
+             email: 'rodrigo@gmail.com',
+             especialidade: 'Reumatologista',
+             crm: '123456'
     assert medico.save
     assert medico.destroy
   end
--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -8,15 +8,15 @@ class PacienteTest < ActiveSupport::TestCase
 
   test "Salvar paciente com todos os atributos validos" do
     paciente = Paciente.new nome: 'Rodrigo',
-                            cpf:'70383167450',
-                            email: "rodrigo@gmail.com",
-                            data_nascimento: "2002-06-05",
-                            endereco_attributes:{ cep: "55298-165",
-                            logradouro: "rua do zekinha",
-                            cidade: 'Garanhuns',
-                            bairro: "Heliopolis",
-                            complemento: 'posto',
-                            }
+               cpf:'70383167450',
+               email: "rodrigo@gmail.com",
+               data_nascimento: "2002-06-05",
+               endereco_attributes:{ cep: "55298-165",
+               logradouro: "rua do zekinha",
+               cidade: 'Garanhuns',
+               bairro: "Heliopolis",
+               complemento: 'posto',
+               }
     assert paciente.save
   end
 end
