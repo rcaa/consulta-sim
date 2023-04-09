d4b8890b450eb7093a87791aa0cecde91ab166f8
RogerioLacerdaGH <nenorogerinho@gmail.com>
2023-04-09 02:40:03 -0300

    test model paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Conta paciente válida' do
+    paciente = Paciente.new nome: 'rogerio',
+                            data_nascimento: '20/03/2000',
+                            cpf: '12015559493',
+                            email: 'paciente@teste.com',
+                            endereco_attributes:{
+                              logradouro: 'Rua teste',
+                              complemento: 'apto 08',
+                              cep: '55022-220',
+                              bairro: 'rendeiras',
+                              cidade: 'caruaru'
+                            }
+    assert_not paciente.save
+
+  end
+
+  test 'Conta paciente sem cpf' do
+    paciente = Paciente.new nome: 'rogerio',
+                            data_nascimento: '20/03/2000',
+                            cpf: '',
+                            email: 'paciente@teste.com',
+                            endereco_attributes:{
+                              logradouro: 'Rua teste',
+                              complemento: 'apto 08',
+                              cep: '55022-220',
+                              bairro: 'rendeiras',
+                              cidade: 'caruaru'
+                            }
+    assert_not paciente.save
+  end
 end
