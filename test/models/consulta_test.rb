0f054cdc30de573160b4b53068ac50778002cecd
RWilker87 <rianwilker17@gmail.com>
2023-04-10 15:59:43 -0300

    testes de model de consulta

--- test/models/consulta_test.rb
+++ test/models/consulta_test.rb
@@ -1,7 +1,60 @@
 require "test_helper"
 
 class ConsultaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Criando Consulta corretamente' do
+    medico = Medico.new nome: 'Rian Wilker Santos',
+                        cpf: '11698885826',
+                        email: 'rian@gmail.com',
+                        especialidade: 'Pediatra',
+                        crm: '000000'
+    assert medico.save
+
+    paciente = Paciente.new
+    paciente.nome = "Maria Lyvia Farias de Santana"
+    paciente.data_nascimento = "25/03/2003"
+    paciente.cpf = "11698885826"
+    paciente.email = "test@gmail.com"
+
+    enderecos = Endereco.new
+    enderecos.logradouro = "Rua"
+    enderecos.complemento = "Posto"
+    enderecos.cep = "55365000"
+    enderecos.bairro = "Centro"
+    enderecos.cidade = "Garanhuns"
+
+    paciente.endereco = enderecos
+    assert paciente.save
+
+    consulta = Consulta.new data: '15/05/2023',
+                            horario:'15:00',
+                            paciente_id: paciente.id,
+                            medico_id: medico.id
+    assert consulta.save
+  end
+
+
+  test 'Criando sem medico' do
+    paciente = Paciente.new
+    paciente.nome = "Maria Lyvia Farias de Santana"
+    paciente.data_nascimento = "25/03/2003"
+    paciente.cpf = "11698885826"
+    paciente.email = "test@gmail.com"
+
+    enderecos = Endereco.new
+    enderecos.logradouro = "Rua"
+    enderecos.complemento = "Posto"
+    enderecos.cep = "55365000"
+    enderecos.bairro = "Centro"
+    enderecos.cidade = "Garanhuns"
+
+    paciente.endereco = enderecos
+    assert paciente.save
+
+    consulta = Consulta.new data: '15/05/2023',
+                            horario:'16:00',
+                            paciente_id: paciente.id
+
+
+    assert_not consulta.save, "consulta sem medico"
+  end
 end
