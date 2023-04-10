545829f31cf74db0476cdf78c07f965520c7dbf6
LuanValentinoS <luanvalentino@gmail.com>
2023-04-10 02:17:07 -0300

    Testes da classe model medico_teste

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,9 +1,25 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  test 'criando medico corretamente' do
-    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
-                        especialidade: 'Reumatologista', crm: '123456'
-    assert medico.save
+  #Para validar que o nome do medico eh obrigatorio e tem tamanho entre 5 e 40 caracteres
+  test 'nome deve estar presente e ter tamanho entre 5 e 40 caracteres' do
+    medico = Medico.new(nome: 'Jose', cpf: '12345678901', email: 'jose@example.com', especialidade: 'Oftalmologia', crm: 12345)
+    assert_not medico.valid?
+    assert_includes medico.errors.details[:nome], { error: :blank }
+
+    medico.nome = 'Jose da Silva Oliveira Santos'
+    assert medico.valid?
+    assert_empty medico.errors.details[:nome]
+  end
+  
+  #Para validar que o CPF do medico eh valido
+  test 'cpf deve estar presente e ser válido' do
+    medico = Medico.new(nome: 'João', cpf: '123.456.789-01', email: 'joao@example.com', especialidade: 'Oftalmologia', crm: 12345)
+    assert_not medico.valid?
+    assert_includes medico.errors.details[:cpf], { error: :cpf_valido }
+
+    medico.cpf = '12345678901'
+    assert medico.valid?
+    assert_empty medico.errors.details[:cpf]
   end
 end
