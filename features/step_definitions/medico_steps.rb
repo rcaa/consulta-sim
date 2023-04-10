80b18505d7cd6fa301fd1c432ca02bbd1f10d2e2
Eliaz500 <eliazzlima@gmail.com>
2023-04-10 14:43:07 -0300

    Teste Cucumber Step Medico

--- features/step_definitions/medico_steps.rb
+++ features/step_definitions/medico_steps.rb
@@ -1,38 +1,28 @@
-Given('estou na pagina de medico') do
-  visit 'medicos/new'
-  expect(page).to have_content('New medico')
+Given('estou na tela de criar Medico') do
+  visit new_medico_path
 end
 
-Given('o medico com CRM {string} existe') do |crm|
-  fill_in 'medico[nome]', :with => 'Rodrigo Andrade'
-  fill_in 'medico[cpf]', :with => '706.508.980-01'
-  fill_in 'medico[email]', :with => 'rodrigo@rodrigo.com'
-  fill_in 'medico[especialidade]', :with => 'Reumatologista'
+When('eu preencho os campos de nome com {string}, com o CPF {string}, com o email {string}, com a Especialidade {string} e com o CRM {string}') do |nome, cpf, email, especialidade, crm|
+
+  #Preenche os campos"
+  fill_in 'medico[nome]', :with => nome
+  fill_in 'medico[cpf]', :with => cpf
+  fill_in 'medico[email]', :with => email
+  fill_in 'medico[especialidade]', :with => especialidade
   fill_in 'medico[crm]', :with => crm
-  click_button 'Create Medico'
-  expect(page).to have_content(crm)
+
 end
 
-When('eu clico em remover o medico com CRM {string}') do |crm|
-  click_button 'Destroy this medico'
-end
-
-Then('eu vejo uma mensagem que o medico foi apagado com sucesso') do
-  expect(page).to have_content('Medico was successfully destroyed')
-end
-
-When('eu preencho os campos de nome {string}, cpf {string}, email {string}, especialidade {string} e crm {string}') do |string, string2, string3, string4, string5|
-  fill_in 'medico[nome]', :with => string
-  fill_in 'medico[cpf]', :with => string2
-  fill_in 'medico[email]', :with => string3
-  fill_in 'medico[especialidade]', :with => string4
-  fill_in 'medico[crm]', :with => string5
-end
-
-When('eu clico em cadastrar novo medico') do
+When('clico em Create Medico') do
   click_button 'Create Medico'
 end
 
-Then('eu vejo uma mensagem que o medico foi cadastrado com sucesso') do
-  expect(page).to have_content('Medico was successfully created.')
+Then('Eu vejo a pagina do Medico {string}') do |nome|
+  # Espero a página do Medico ser carregado
+  expect(page).to have_content(nome)
+end
+
+Then('Eu vejo a mesagem {string}') do |mensagem|
+  # Espero a página do paciente ser carregado
+  expect(page).to have_content(mensagem)
 end
