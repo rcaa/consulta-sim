9298fad9f8d1fcf608d4b1ee9dfc04b40b1223cf
RWilker87 <rianwilker17@gmail.com>
2023-04-10 15:42:29 -0300

    testes de model de paciente

--- test/models/paciente_test.rb
+++ test/models/paciente_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class PacienteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  paciente = Paciente.new
+  paciente.nome = "Maria Lyvia Farias de Santana"
+  paciente.data_nascimento = "25/03/2003"
+  paciente.cpf = "11698885826"
+  paciente.email = "test@gmail.com"
+
+  enderecos = Endereco.new
+  enderecos.logradouro = "Rua"
+  enderecos.complemento = "Posto"
+  enderecos.cep = "55365000"
+  enderecos.bairro = "Centro"
+  enderecos.cidade = "Garanhuns"
+
+  paciente.endereco = enderecos
+  assert paciente.save
+  test "Criar paciente sem cpf" do
+    paciente = Paciente.new
+    paciente.nome = "Rian Wilker Santos Melo"
+    paciente.data_nascimento = "20/11/2001"
+    paciente.cpf = ""
+    paciente.email = "teste@gmail.com"
+
+    enderecos = Endereco.new
+    enderecos.logradouro = "Rua"
+    enderecos.complemento = "Rua da caixa"
+    enderecos.cep = "55365000"
+    enderecos.bairro = "Centro"
+    enderecos.cidade = "Capoeiras"
+
+    paciente.endereco = enderecos
+    assert_not paciente.save, "Criou paciente sem CPF"
+  end
 end
